f465004720427a08ea429c035f759fb65525f311
WitherG0 <91891123+WitherG0@users.noreply.github.com>
2021-10-17 16:18:52 +0300

    Add files via upload

---
+++ Задание1/1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, c;
+            Console.WriteLine("Введите дробное число(с запятой):");
+            a = float.Parse(Console.ReadLine());
+            int b = (int)a;
+            c = (a - b) * 10;
+            c = (int)c;
+            Console.WriteLine($"0.x = {c}");
+        }
+    }
+}
